47dd79b441487b68f0955993813bb80423db0baa
AngeloMaiuri <63823104+AngeloMaiuri@users.noreply.github.com>
2023-10-25 11:34:08 +0300

    fix: added delay, updated permissions

--- content.js
+++ content.js
@@ -1,6 +1,15 @@
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   if (message.action === "copyText") {
     const selectedText = window.getSelection().toString();
-    navigator.clipboard.writeText(selectedText);
+    setTimeout(() => {
+      navigator.clipboard
+        .writeText(selectedText)
+        .then(() => {
+          console.log("Text successfully copied.");
+        })
+        .catch((err) => {
+          console.error("Unable to write to clipboard:", err);
+        });
+    }, 200);
   }
 });
